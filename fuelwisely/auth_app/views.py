ec13b00069ed7fd0b3e73038fd39d796e0024055
Aadityadav <aadit.211602@ncit.edu.np>
2025-01-12 10:41:42 +0545

    beforeurls

--- fuelwisely/auth_app/views.py
+++ fuelwisely/auth_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Register(request):
+    return render(request, 'Register.html')
+
+def logIN(request):
+    return render(request, 'logIN.html')
+
