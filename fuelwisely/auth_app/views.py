7e88e8928bd981c9ebed30a3bd204ecf596ef800
Nirbhay <nirbhay.211631@ncit.edu.np>
2025-01-12 10:36:15 +0545

    edit links

--- fuelwisely/auth_app/views.py
+++ fuelwisely/auth_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+
+# register user
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,"username already exists")
+            elif User.object.filter(email=email).exists():
+                messages.error(request,"email already exists")
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                messages.success(request,"User registered successfully! ")
+                return redirect('login')
+        else:
+            message.error(request,"Passwords do not match")
+        return render(request,'register.html')
+    
+
+                
+
+                
+        
+    
+    
+    
+    
--- fuelwisely/fuelwisely/settings.py
+++ fuelwisely/fuelwisely/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profile_app',
+    'core',
+    'jazzmin',
+    
 ]
 
 MIDDLEWARE = [
