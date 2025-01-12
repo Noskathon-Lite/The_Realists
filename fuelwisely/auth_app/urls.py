d475c045d5d0efce41c5149c643fd2ef35ac4a1c
Aadityadav <aadit.211602@ncit.edu.np>
2025-01-12 15:00:23 +0545

    Login and registers

--- fuelwisely/auth_app/urls.py
+++ fuelwisely/auth_app/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 
 urlpatterns = [
+     path('', register_user, name='register'),
     path('register/', register_user, name='register'),
+    path('login/', login_user, name='Login'),
     # path('login/', views.login_user, name='login'),
     # path('logout/', views.logout_user, name='logout'),
     # path('dashboard/', views.dashboard, name='dashboard'),  
