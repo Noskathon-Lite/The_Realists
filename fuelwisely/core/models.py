53c8bdf503ac16c75c897bb470f2a0b12515901f
Aashish <aashish.211607@ncit.edu.np>
2025-01-12 15:45:10 +0545

    a

--- fuelwisely/core/models.py
+++ fuelwisely/core/models.py
@@ -1,3 +1,53 @@
 from django.db import models
-
+from django.db.models import Sum
 # Create your models here.
+class  Transportationmodel(models.Model):
+    vehicle_type = models.CharField(max_length=50,choices=
+                                       [
+                                        ('car','Car'),
+                                        ('bike','Bike'),
+                                        ('bus','Bus'),
+                                        ('scooter','Scooter'),
+                                       ]
+                                    )
+    distance = models.FloatField(help_text = "Distance travelled in kilomeeters" )
+
+    mode = models.CharField(max_length=50, choices=
+                            [
+                                ('public','Public'),
+                                ('private','Private'),
+                            ]
+                            )
+    vehicle_size = models.CharField(max_length=50, choices=[
+        ('small', 'Small'),
+        ('average', 'Average'),
+        ('large', 'Large'),
+        ('big', 'Big'),
+    ], help_text ="Size category of the vehicle")
+    emissions = models.FloatField(help_text = "Co2 emmissions in grams ")
+
+    def __str__(self):
+       return f"Transportation_Log:{self.vehicle_type} ({self.vehicle_size}) - {self.mode}"
+
+
+
+class EmissionsSummary(models.Model):
+    total_distance  = models.FloatField(default=0.0, help_text = "total distance traveled in kilomeeters")
+    total_emissions = models.FloatField(default=0.0,help_text = "total co2 emissions ")
+
+    def __str__(self):
+        return f"Distance :{self.total_distance} km , Emissions : {self.total_emissions}" 
+
+    @classmethod
+    def calculate_totals(cls):
+        
+        total_distance  =  Transportationmodel.objects.aggregate(Sum('distance'))['distance__sum'] or 0.0
+        total_emissions = Transportationmodel.objects.aggregate(Sum('emissions'))['emissions__sum'] or 0.0
+
+                
+        summary, created = cls.objects.get_or_create(id=1)
+        summary.total_distance = total_distance
+        summary.total_emissions = total_emissions
+        summary.save()
+
+        return summary
--- fuelwisely/fuelwisely/settings.py
+++ fuelwisely/fuelwisely/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'core',
+    'auth_app',
+    'profile_app',
+    'jazzmin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
